ada1356568a2fadb8fb9b3a388366645250b3a4a
Derecky Costa <dereckycosta@gmail.com>
2022-03-08 18:41:22 -0300

    chore: add files to coverage ignore

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,7 +7,7 @@ import { render, RenderResult, fireEvent, waitFor } from '@testing-library/react
 
 import { ValidationStub, AuthenticationSpy } from '@/presentation/test'
 
-import Login from './login'
+import { Login } from '@/presentation/pages'
 import { InvalidCredentialsError } from '@/domain/errors'
 
 
