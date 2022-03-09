658e070a7d68d690773cbb932561c94ec0822003
Derecky Costa <dereckycosta@gmail.com>
2022-03-08 21:52:47 -0300

    chore: add coveralls

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
 module.exports = {
-	roots: ['<rootDir>/src'],
-	collectCoverageFrom: [
-		'<rootDir>/src/**/*.{ts,tsx}',
-		'!<rootDir>/src/main/**/*',
-		'!<rootDir>/src/presentation/components/router/**/*',
-		'!<rootDir>/src/**/index.ts',
-		'<rootDir>/src/main/index.ts',
-		'!**/*.d.ts'
-	],
-	coverageDirectory: 'coverage',
-	testEnvironment: 'jsdom',
-	transform: {
-		'.+\\.(ts|tsx)$': 'ts-jest'
-	},
-	moduleNameMapper: {
-		'@/(.*)': '<rootDir>/src/$1',
-		'\\.scss$': 'identity-obj-proxy'
-	}
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
 }
